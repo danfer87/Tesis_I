a2219bb91f7d366d3419304dfa6360db42e7ac5d
kleber <kleber@kleber-PC>
2014-12-17 16:11:17 -0500

    cambios en ff

--- test/AdministrarRubros.java
+++ test/AdministrarRubros.java
@@ -3,8 +3,15 @@
  * and open the template in the editor.
  */
 
+import ec.com.sisapus.dao.rubroDao;
+import ec.com.sisapus.daoimpl.rubroDaoImpl;
+import ec.com.sisapus.daoimpl.usuarioDaoImpl;
+import ec.com.sisapus.modelo.Rubro;
+import java.util.List;
+import javax.annotation.Resource;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -18,6 +25,13 @@ public class AdministrarRubros {
     
     public AdministrarRubros() {
     }
+     @Resource  
+    List<Rubro> listarubros;  
+   Rubro rubro;
+    
+    rubroDao rubrodao= new rubroDaoImpl();
+    
+    private static final int Rubrobd = 1;  
     
     @BeforeClass
     public static void setUpClass() {
@@ -29,6 +43,10 @@ public class AdministrarRubros {
     
     @Before
     public void setUp() {
+        rubro=new Rubro();
+        this.rubro.setNombreRubro("ESTRUCTURA TRAMO 1");
+        this.rubro.setDetalleRubro("ESTRUCTURA TRAMO 1 CIUDAD DE QUITO");
+        this.rubro.setUnidadRubro("METROS CUBICOS");
     }
     
     @After
@@ -37,6 +55,55 @@ public class AdministrarRubros {
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-    // @Test
-    // public void hello() {}
+    
+    
+    public void BuscarRubros()
+           {
+         listarubros=rubrodao.buscarTodosRubros();
+          Assert.assertTrue("Busca en la base de datos que exista  registros de Rubros",  
+          listarubros.size() == Rubrobd);  
+           }  
+    @Test
+       public void CrearRubros() {
+          try {
+    //crear usuario
+         rubrodao.crearRubro(rubro); 
+       if(rubrodao !=null) {
+ 
+         assertTrue("Rubro Creado Exitosamente", true);
+
+         } else {
+
+           fail("Error Rubro no creado" );
+    }
+
+   } catch (Exception e) {
+
+ fail("Error Rubro no creado");
+ } 
+   }  
+    
+    @Test   
+public void EliminarRubro() {
+ try {
+ rubrodao.eliminarRubro(rubro.getCodigoRubro());   
+if(rubrodao !=null) {
+ 
+assertTrue(" Registro Eliminado Exitosamente", true);
+
+} else {
+
+   fail("Error Rubro no se puede eliminar" );
+    }
+
+} catch (Exception e) {
+
+fail("Rubro no eliminado");
+} 
+   } 
+    
+    
+    
+    
+    
 }
