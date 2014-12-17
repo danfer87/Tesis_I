f117dcfad176b8a5bdfb8bd97089e27527e92ea3
kleber <kleber@kleber-PC>
2014-12-17 17:38:22 -0500

    cambios en ff

--- test/Administrar_Materiales.java
+++ test/Administrar_Materiales.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.annotation.Resource;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -30,7 +31,7 @@ public class Administrar_Materiales {
      Categoriamaterial catmaterial;
     materialDao materialdao= new materialDaoImpl();
     
-    private static final int manobd = 3; 
+    private static final int materialbd = 2; 
     @BeforeClass
     public static void setUpClass() {
     }
@@ -41,6 +42,13 @@ public class Administrar_Materiales {
     
     @Before
     public void setUp() {
+        material=new Material();
+        catmaterial=new Categoriamaterial();
+        material.setNombreMat("cable tipo FTO cat6");
+        material.setUnidMat("MT");
+        material.setPrecunitMat(1.70);
+        material.setCategoriamaterial(catmaterial);
+        catmaterial.setCodCatMat(1);
     }
     
     @After
@@ -49,6 +57,70 @@ public class Administrar_Materiales {
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void BuscarManoObra()
+           {
+       listarmaterial=materialdao.buscarTodosMater();
+          Assert.assertTrue("Busca en la base de datos que exista 2 registros para Mantenimiento de Materiales",  
+         listarmaterial.size() == materialbd);  
+           }  
+    @Test
+       public void CrearMaterial() {
+          try {
+    //crear     
+         materialdao.crearMater(material);
+       if(materialdao !=null) {
+ 
+         assertTrue("Registro para Material Creado Exitosamente", true);
+
+         } else {
+
+           fail("Error No se pudo Crear Registro para Material" );
+    }
+
+   } catch (Exception e) {
+
+ fail("Error Registro para Material");
+ } 
+   }  
+    
+    @Test   
+public void EliminarMaterial() {
+ try {
+    materialdao.eliminarMater(material.getCodigoMat());
+if( materialdao !=null) {
+ 
+assertTrue(" Registro Eliminado Exitosamente", true);
+
+} else {
+
+   fail("Error Registro no se puede eliminar" );
+    }
+
+} catch (Exception e) {
+
+fail("Registro no eliminado");
+} 
+   } 
+    
+ @Test   
+ public void ActualizarMaterial() {
+   try {
+    
+materialdao.actualizarMater(material);
+if(materialdao !=null) {
+ 
+assertTrue(" Registro Actualizado Exitosamente", true);
+
+} else {
+
+fail("Error Registro no se pudo actualizar" );
+}
+
+} catch (Exception e) {
+
+fail("Error Registro no se pudo Actualizar");
+} 
+   } 
+    
 }
